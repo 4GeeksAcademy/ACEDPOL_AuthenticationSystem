2dbd471bc9a5f7e17e70bb321a2e543c5da455b7
P. Pablo Cubells Talavera <pablocub@ucm.es>
2025-03-03 09:54:06 +0000

    Navbar link: Private => deshabilitado sin usuario activo

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import useGlobalReducer from '../hooks/useGlobalReducer';
 import '../assets/styles/navbar.css'  // Global styles for your application
 
 export const Navbar = () => {
     const location = useLocation();
+    const { store } = useGlobalReducer();
 
     return (
         <div className="d-flex justify-content-center mt-2">
@@ -18,7 +20,13 @@ export const Navbar = () => {
                     <Link className={`nav-link ${location.pathname === "/signup" && "active"}`} to="/signup">Registrar</Link>
                 </li> 
                 <li className="nav-item">
-                    <Link className={`nav-link ${location.pathname === "/private" && "active"}`} to="/private">Zona Privada</Link>
+                    <Link 
+                        className={`nav-link ${location.pathname === "/private" && "active"} ${store.token === null && "disabled"}`} 
+                        to={store.token !== null ? "/private" : "#"}
+                        aria-disabled={store.token !== null ? "false" : "true"}
+                    >
+                        Zona Privada
+                    </Link>
                 </li> 
             </ul>
         </div>
